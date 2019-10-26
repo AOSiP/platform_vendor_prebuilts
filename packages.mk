fa60d5820689514d45c0a20952a34cee0f0e5ae1
shagbag913 <sh4gbag913@gmail.com>
2019-10-26 00:02:44 -0400

    Add PixelSetupWizardOverlay
     * Disable AOD on Pixel 1
     * Disable wave gesture & smart AOD on Pixel 4
    
    Change-Id: Ia940b9fc5006c782be154ddf036491490d240037

--- packages.mk
+++ packages.mk
@@ -6,3 +6,13 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/prebuilts/overlay
 
 # Extra packages
 PRODUCT_PACKAGES += \
+    PixelSetupWizardOverlay
+
+# Setup Wizard props
+PRODUCT_PRODUCT_PROPERTIES += \
+    setupwizard.theme=glif_v3_light \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    ro.setupwizard.esim_cid_ignore=00000001 \
