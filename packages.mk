2892beb9225fe7e83bd8e3cfcee5fffe1cdaeeea
Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
2020-02-23 10:29:21 -0500

    prebuilts: redo for gapp builds
    Change-Id: If4c03139566744ae4a82b5bb43d7f389343a3480

--- packages.mk
+++ packages.mk
@@ -1,4 +1,8 @@
+ # Prebuilts
+DEVICE_PACKAGE_OVERLAYS += vendor/prebuilts/overlay
+
+# Allow overlays to be excluded from enforcing RRO
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/prebuilts/overlay
+
 # Extra packages
 PRODUCT_PACKAGES += \
-    LatinIMEGooglePrebuilt \
-    SoundPickerPrebuilt
